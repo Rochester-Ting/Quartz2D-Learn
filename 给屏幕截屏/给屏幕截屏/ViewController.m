6143e958d018ffac99a32b6a6d81c6c58cebaf15
Rochester Ting <694526771@qq.com>
2016-09-30 23:20:10 +0800

    给手机屏幕截图

--- 给屏幕截屏/给屏幕截屏/ViewController.m
+++ 给屏幕截屏/给屏幕截屏/ViewController.m
@@ -18,7 +18,16 @@
     [super viewDidLoad];
 }
 -  (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    
+//    开启位图上下文
+    UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, NO, 0);
+//    将屏幕界面加载到上下文中
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    [self.view.layer renderInContext:ctx];
+//    获取一张新的图片
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+//    将图片保存到桌面上
+    NSData *data = UIImagePNGRepresentation(newImage);
+    [data writeToFile:@"/Users/Rochester/Desktop/tu.png" atomically:YES];
 }
 
 - (void)didReceiveMemoryWarning {
